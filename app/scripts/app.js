dff34e369992518af4287e7a0572675b53388160
Sanjib Paudel <sanjibpaudel@Sanjibs-MacBook-Pro.local>
2015-03-26 17:55:03 -0400

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularChartApp
+ * @description
+ * # angularChartApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularChartApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'chart.js'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
